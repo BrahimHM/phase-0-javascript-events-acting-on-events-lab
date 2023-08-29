ad5452c96e38d1d60dbbd646df49b2d011a612e0
Brahim HM <brahimhm98@gmail.com>
2023-08-29 23:19:02 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const moveDodgerLeft=()=>{
+    const dodger = document.getElementById('dodger')
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if(left>0){
+    dodger.style.left = `${left - 1}px`;
+    }else{
+    dodger.style.left = `${left}px`;
+    }  
+
+}
+
+const moveDodgerRight=()=>{
+    const dodger = document.getElementById('dodger')
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if(left<360){
+        dodger.style.left = `${left + 1}px`;
+    }else{
+        dodger.style.left = `${left}px`;
+    }
+}
+            
+      
